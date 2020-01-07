c9043841e18a828ddcdc3277beb67b7329892be9
steven oakley <stevenoakley211@live.com>
2020-01-06 19:52:37 -0600

    building login in form

--- farm-fresh/src/components/LoginForm.js
+++ farm-fresh/src/components/LoginForm.js
@@ -1,4 +1,45 @@
+import React, {useState, useEffect} from "react";
+import {withFormik,Form,Field} from "formik";
+import * as Yup from "yup";
+import axios from "axios";
 
+const LoginForm = ({values,errors,touched,status}) => {
+
+    const [user,setUser] =useState([])
+    // local stat that holds the succesful form Submission.
+    useEffect(()=>{
+        status && setUser(user => [...user,status]);
+    },[status]);
+
+    return (
+        <div>
+            <Form>
+                <label htmlFor="userName"> 
+                Login
+                    <Field
+                        id="userName"
+                        type="text"
+                        name="userName"
+                        placeholder="username"
+                    />
+                </label>
+                <label>
+                    Password
+                    <Field 
+                        id="password"
+                        type ="text"
+                        name = "password"
+                        placeholder = "Password"
+                    />
+                </label>
+            </Form>
+        </div>
+    );
+};
+
+const FomrikLoginForm = withFormik ({
+
+})
 // Needs Encryption
 // Username
 // Password
